a4f55d47ef5380f7678a36894c4caa9699d3e4ef
Bhavesh-0909 <bhaveshchoudhary0909@gmail.com>
2024-01-17 21:36:37 +0530

    authonitication

--- controller/login.controller.js
+++ controller/login.controller.js
@@ -23,15 +23,31 @@ exports.login = async(req, res) => {
                 id:user._id,
                 role:user.role
             }
+
             if(await bcrypt.compare(password, user.password)){
 
-                const token = jwt.sign(playload, process.env.JWT_SECRET)
+                const token = jwt.sign(playload, process.env.JWT_SECRET, {expiresIn:"2h",});
+
+                user.token = token;
+                user.password = undefined;
+
+                const options = {
+                    expiresIn: new Date(Date.now() + 3*24*60*60*1000),
+                    httpOnly:true,
+                }
+
+                res.cookie("token", token, options).status(200).json({
+                    token,
+                    user,
+                    message:"user logind"
+                })
 
             }else{
                 return res.status(400).json({
                     message:"Password did'nt match"
                 })
             }
+
         }
         else{
             return res.status(400).json({
