06b174543cdab9785082eb3dfbbcc2f68cb6ecc9
Bhavesh-0909 <bhaveshchoudhary0909@gmail.com>
2024-01-18 22:37:13 +0530

    cookie parser added

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 require('dotenv').config();
 const dbConnect = require('./db/database');
 const routes = require('./routes/user.route')
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(PORT, ()=>{
     console.log(`Server Started at ${PORT}`);
